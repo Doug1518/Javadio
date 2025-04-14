8f63da4e9abf6dc707ab14c6ae2a5c6c1f8f90fc
Douglas Augusto <149178392+Doug1518@users.noreply.github.com>
2025-04-14 18:53:57 -0300

    adicionando interface gra´fica
    Foram criados para interface gráfica do projeto

---
+++ src/main/java/sudoku/ui/custom/buttom/ResetButton.java
@@ -0,0 +1,13 @@
+package sudoku.ui.custom.buttom;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class ResetButton extends JButton {
+
+    public ResetButton(final ActionListener actionListener) {
+        this.setText("Reiniciar jogo");
+        this.addActionListener(actionListener);
+    }
+
+}
